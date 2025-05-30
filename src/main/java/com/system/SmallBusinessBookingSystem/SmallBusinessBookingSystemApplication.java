63649e30594fd769e651d0b2e1738a892a44c2c4
Taras Shevchuk <taras.shevchuk@Codillass-Air.box>
2025-05-30 13:51:41 +0200

    first commit

---
+++ src/main/java/com/system/SmallBusinessBookingSystem/SmallBusinessBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.SmallBusinessBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBusinessBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallBusinessBookingSystemApplication.class, args);
+	}
+
+}
