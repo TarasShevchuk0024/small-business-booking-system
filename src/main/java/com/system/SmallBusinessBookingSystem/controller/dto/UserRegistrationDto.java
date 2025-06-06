ae17aedaaa825be585facd566d9e8cda10dae225
Taras Shevchuk <taras.shevchuk@Codillass-Air.box>
2025-06-06 21:34:24 +0200

    Add changes

--- src/main/java/com/system/SmallBusinessBookingSystem/controller/dto/UserRegistrationDto.java
+++ src/main/java/com/system/SmallBusinessBookingSystem/controller/dto/UserRegistrationDto.java
@@ -2,9 +2,11 @@ package com.system.SmallBusinessBookingSystem.controller.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRegistrationDto {
     private final Integer id;
